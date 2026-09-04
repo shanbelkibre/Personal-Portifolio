a720ba0b46f57dba8fc670766fc01f4ee745dcfe
shanbelkibre <shambel5110@gmail.com>
2026-09-04 11:47:04 +0300

    update the seed data and adminui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <AdminCMSDrawer />
-        <BrowserRouter>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <KeyboardShortcuts />
           <Routes>
             <Route path="/" element={<Index />} />
